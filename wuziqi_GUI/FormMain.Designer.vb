b8a777cb996faa854ca7470ce994ad6424169773
SMagic-L <38354551+SMagic-L@users.noreply.github.com>
2020-04-24 17:33:34 +0800

    Updated GUI

--- wuziqi_GUI/FormMain.Designer.vb
+++ wuziqi_GUI/FormMain.Designer.vb
@@ -22,17 +22,32 @@ Partial Class FormMain
     '不要使用代码编辑器修改它。
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.PanelChessBoard = New System.Windows.Forms.Panel()
         Me.SuspendLayout()
+        '
+        'PanelChessBoard
+        '
+        Me.PanelChessBoard.BackgroundImage = Global.wuziqi_GUI.My.Resources.chessboard.board
+        Me.PanelChessBoard.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PanelChessBoard.Location = New System.Drawing.Point(49, 47)
+        Me.PanelChessBoard.Name = "PanelChessBoard"
+        Me.PanelChessBoard.Size = New System.Drawing.Size(687, 679)
+        Me.PanelChessBoard.TabIndex = 0
         '
         'FormMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1012, 565)
+        Me.BackColor = System.Drawing.Color.LightGray
+        Me.ClientSize = New System.Drawing.Size(1127, 776)
+        Me.Controls.Add(Me.PanelChessBoard)
         Me.MaximizeBox = False
         Me.Name = "FormMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "五子棋"
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents PanelChessBoard As Panel
 End Class
--- wuziqi_GUI/FormMain.vb
+++ wuziqi_GUI/FormMain.vb
@@ -1,53 +1,97 @@
 ﻿Public Class FormMain
-    Const BOARD_BLOCK_SIZE As Integer = 30
-    Const BOARD_BLOCK_MARGIN As Integer = 32
-    Private opt As Integer = 0
-    Private counter As Integer = 1
+    'global and const
+    Const BOARD_BLOCK_SIZE As Integer = 40
+    Const BOARD_INNER_MARGIN As Integer = 0
+    Const BOARD_OUTER_MARGIN As Integer = 40
+    Private Property Opt As ChessColor = ChessColor.Black
+    Private Property Last_i As Integer = -1
+    Private Property MoveCounter As Integer = 1
+    Private currentBoard(224) As Integer
+    Enum ChessColor
+        White = 1
+        Black = 2
+    End Enum
+
+    'out library
+
+    Public Declare Function getCounter Lib "Dll_Test.dll" () As Integer
+
+
 
     Private Sub FormMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'lock window
         Me.MaximumSize = Me.Size
         Me.MinimumSize = Me.Size
 
+        'initialize chessboard
         InitializeChessboard()
+
     End Sub
 
     Sub InitializeChessboard()
         Dim tmp As MyButton
+        PanelChessBoard.Height = 15 * BOARD_BLOCK_SIZE + 14 * BOARD_INNER_MARGIN + 2 * BOARD_OUTER_MARGIN
+        PanelChessBoard.Width = PanelChessBoard.Height
+
         For i = 0 To 224
             tmp = New MyButton With {
                 .Text = "",
-                .BackColor = Color.Blue,
+                .BackColor = Me.BackColor,
+                .BackgroundImage = My.Resources.chessboard.empty,
                 .Index = i,
-                .Left = 30 + (i Mod 15) * BOARD_BLOCK_MARGIN,
-                .Top = 30 + (i \ 15) * BOARD_BLOCK_MARGIN,
+                .Left = BOARD_OUTER_MARGIN + (i Mod 15) * (BOARD_INNER_MARGIN + BOARD_BLOCK_SIZE),
+                .Top = BOARD_OUTER_MARGIN + (i \ 15) * (BOARD_INNER_MARGIN + BOARD_BLOCK_SIZE),
                 .Size = New Point(BOARD_BLOCK_SIZE, BOARD_BLOCK_SIZE),
-                .FlatStyle = FlatStyle.Flat
+                .FlatStyle = FlatStyle.Flat,
+                .BackgroundImageLayout = ImageLayout.Stretch
             }
+            tmp.FlatAppearance.BorderColor = Me.BackColor
             AddHandler tmp.Click, AddressOf ChessBoard_Click
-            ChessBoard(i) = tmp
-            Me.Controls.Add(ChessBoard(i))
+            BtnChessBoard(i) = tmp
+            PanelChessBoard.Controls.Add(tmp)
         Next
+
     End Sub
 
     Private Sub ChessBoard_Click(sender As Object, e As EventArgs)
-        If opt Mod 2 = 0 Then
-            ChessBoard(sender.Index).BackColor = Color.Black
-            ChessBoard(sender.Index).ForeColor = Color.White
-        Else
-            ChessBoard(sender.Index).BackColor = Color.White
-            ChessBoard(sender.Index).ForeColor = Color.Black
+        Dim i As Integer = sender.Index
+        If BtnChessBoard(i).Text <> "" Then
+            Exit Sub
         End If
-        ChessBoard(sender.Index).Text = counter.ToString()
-        opt += 1
-        counter += 1
+        currentBoard(i) = Opt
+        UpdateBtnBoard(i)
+        MoveCounter += 1
     End Sub
 
-    Friend ChessBoard(224) As MyButton
+    Private Sub UpdateBtnBoard(i As Integer)
+        If Last_i <> -1 Then
+            BtnChessBoard(Last_i).FlatAppearance.BorderColor = BtnChessBoard(i).BackColor
+        End If
+        Last_i = i
+
+        If Opt = ChessColor.Black Then
+            'black chess 
+            BtnChessBoard(i).ForeColor = Color.White
+            BtnChessBoard(i).BackgroundImage = My.Resources.chessboard.black
+            Opt = ChessColor.White
+        Else
+            'white chess
+            BtnChessBoard(i).ForeColor = Color.Black
+            BtnChessBoard(i).BackgroundImage = My.Resources.chessboard.white
+            Opt = ChessColor.Black
+        End If
+        BtnChessBoard(i).Text = MoveCounter.ToString()
+        BtnChessBoard(i).FlatAppearance.BorderColor = Color.Blue
+    End Sub
+
+    Friend BtnChessBoard(224) As MyButton
+
+
 
     Class MyButton
         Inherits Button
         Property Index As Integer
     End Class
+
 
 End Class
