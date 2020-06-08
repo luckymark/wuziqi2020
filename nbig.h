e7a78d91e3ce7a7632b025fa0a9142bc806a18f1
itungsten <tungstenig@gmail.com>
2020-06-08 18:26:50 +0800

    change the behaviour of NBIG, and accelarate the search

--- nbig.h
+++ nbig.h
@@ -20,39 +20,46 @@ struct Node{
 
 
 struct NBig{
+    //UPD: 更改最初栈的实现方式为有限长度优先队列
+    //有限长度是为了减少不必要的中间节点，即减少每层搜索数目
+    //优先队列FIFO相比栈的FILO的好处在于贪心最优的点最先被搜索，有利于alpha-beta剪枝
+    //实测常数减少为0.5~0.8
+
+
     //经过一些测试得出，在和大佬下的时候，实际上合理的走法是很少的
     //保留前N个较大的数，这是为了减少不合理的中间节点
 #define N 3
     //经过测试，3看上去足够了
-    NBig():_size(0)
+    NBig():_beg(0),_end(0)
     {
         //NBig是一个容器
     }
     int size()const {
-        return _size;
+        return _end-_beg;
     }
     void push(const Node& x){
         //偷懒使用优先队列来维护单调性，虽然这样实现看上去效率不高，但是占总体开销很小可以忽略。
         std::priority_queue<Node> Q;
-        for(int i=0;i<_size;++i)Q.push(arr[i]);
+        for(int i=0;i<_end;++i)Q.push(arr[i]);
         Q.push(x);
         //把原有的元素和新元素放入优先队列
-        _size=0;
-        while(!Q.empty()&&_size<N){
-            arr[_size++]=Q.top();
+        _end=0;
+        while(!Q.empty()&&_end<N){
+            arr[_end++]=Q.top();
             Q.pop();
         }
         //从优先队列中取出元素
     }
     const Node& pop(){
-        return arr[--_size];
+        return arr[_beg++];
     }
     void clear(){
-        _size=0;
+        _beg=_end=0;
     }
 private:
     Node arr[N];
-    int _size;
+    int _end;
+    int _beg;
 #undef N
 };
 #endif // BIGN_H
